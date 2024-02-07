6f045b86b1ab55006e197b9051d823c089c956b6
aleksandr <sanuyOKmaklaren0@gmail.com>
2024-02-08 03:44:04 +0500

    feat: 15 passing

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -166,8 +166,11 @@ function getAverage(arr) {
  *    isSameLength(['orange', 'banana', 'cherry']) => true
  *    isSameLength(['cat', 'dog', 'elephant']) => false
  */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  const ownLength = arr[0].length;
+  return arr.every((element) => {
+    return element.length === ownLength;
+  });
 }
 
 /**

c33fb32b7d14a600ec7ee0bb52c4b637c529b420
aleksandr <sanuyOKmaklaren0@gmail.com>
2024-02-08 02:21:01 +0500

    fix: optimal implementation of findElement

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -61,15 +61,7 @@ function sumArrays(arr1, arr2) {
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
 function findElement(arr, value) {
-  let findIndex = -1;
-  function isWantedGuest(element) {
-    const needElem = value;
-
-    return element === needElem;
-  }
-
-  findIndex = arr.findIndex(isWantedGuest);
-  return findIndex;
+  return arr.indexOf(value);
 }
 
 /**

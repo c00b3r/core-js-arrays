eb32789bca5517c04b646abf9bf41802cc2a5f2e
aleksandr <sanuyOKmaklaren0@gmail.com>
2024-02-08 23:18:25 +0500

    feat: 40 passing

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -459,8 +459,16 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array(n)
+    .fill(0)
+    .map((elem, i) => {
+      return Array(n)
+        .fill(0)
+        .map((elem2, j) => {
+          return 1 - Math.min(Math.abs(i - j), 1);
+        });
+    });
 }
 
 /**

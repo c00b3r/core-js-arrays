81867ca1280a9bc84017d85e471a3cfb77abf294
aleksandr <sanuyOKmaklaren0@gmail.com>
2024-02-08 23:02:37 +0500

    feat: 35 passing

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -357,8 +357,15 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  return arr.reduce((balance, record) => {
+    const [income, expense] = record;
+    return balance + (income - expense);
+  }, 0);
 }
 
 /**

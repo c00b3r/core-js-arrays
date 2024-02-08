c59f6570165d09cf7220fcaa5a145dd82de376f7
aleksandr <sanuyOKmaklaren0@gmail.com>
2024-02-08 23:21:39 +0500

    feat: 41 passing task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -482,8 +482,15 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .map((element, index) => {
+      if (element % 2 === 1) {
+        return index;
+      }
+      return undefined;
+    })
+    .filter((index) => index !== undefined);
 }
 
 /**
